13a41c5e3cc78a18e9d769b5858f78d0903ccab9
Sotaro Kaneda <sotarokaneda@Sotaros-MacBook-Air.local>
2023-07-31 12:58:32 -0400

    add datacenter files

--- datacenter.py
+++ datacenter.py
@@ -1,2 +1,12 @@
-
 import csv
+import pandas as pd
+
+# returns carbon emissions in kgs
+def calc_co2(region, accelerator_energy):
+    datacenter_df = pd.read_csv('./data/impact.csv')
+    region_row = datacenter_df.loc[datacenter_df['region'] == region]
+    #impact is gCO2/kWh
+    return region_row['impact'].values[0] / 1000 * accelerator_energy
+
+
+calc_co2('asia-east1', 234234)
