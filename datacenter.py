c9f48506e85a752a6abed2705552b3c120955fa6
Sotaro Kaneda <sotarokaneda@Sotaros-MacBook-Air.local>
2023-07-31 14:30:30 -0400

    organized datacenter.py

--- datacenter.py
+++ datacenter.py
@@ -1,9 +1,14 @@
 import csv
 import pandas as pd
 
+def impact(provider, region):
+  impact_df = pd.read_csv('./data/impact.csv')
+  datacenter = impact_df.loc[(impact_df['provider']==provider) & (impact_df['region']==region)]
+  offset = datacenter["offsetRatio"]  # percentage of carbon emissions the provider offsets
+  impact = datacenter["impact"]       # gCO2eq / kWh
+  return impact.loc[datacenter.index[0]]
+
 # returns carbon emissions in kgs
-def calc_co2(region, accelerator_energy):
-    datacenter_df = pd.read_csv('./data/impact.csv')
-    region_row = datacenter_df.loc[datacenter_df['region'] == region]
+def calc_co2(provider, region, accelerator_energy):
     #impact is gCO2/kWh
-    return region_row['impact'].values[0] / 1000 * accelerator_energy
+    return impact(provider, region) / 1000 * accelerator_energy
--- gpu_training.py
+++ gpu_training.py
@@ -1,1 +1,12 @@
 import csv
+import json
+from model import Model
+import pandas as pd
+
+class Gpu_train:
+    def __init__(self, model, gpu, num_gpu):
+        gpu_df = pd.read_csv('./data/impact.csv')
+        self.model = model
+        self.gpu_flops = 
+        self.num_gpu = num_gpu
+        return
